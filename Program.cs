ed94e8a2ee16d89075d4d1e2664de4c1137d9def
Hugo Jacome <hugo.jacome.viera@gmail.com>
2026-05-04 16:37:00 -0500

    Configure PostgreSQL and Docker deploy

--- Program.cs
+++ Program.cs
@@ -21,9 +21,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseSqlServer(
+//        builder.Configuration.GetConnectionString("DefaultConnection")
+//    ));
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnectionPSQL")
     ));
 
 builder.Services.AddScoped<AgencyRepository>();
